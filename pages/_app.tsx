19fa5cfa7a7ecdd8ea7ded3e48dd6803b3b42114
denghua <denghua.fm@foxmail.com>
2023-04-14 00:13:17 +0800

    add compoents

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,19 @@ import type { AppProps } from 'next/app'
 
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
+import Model from '@/components/Model'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Model
+        isOpen
+        title="Test Model"
+        actionLabel='Submit'
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
